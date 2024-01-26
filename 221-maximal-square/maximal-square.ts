0a7c2a066d32b803e0f4b375a9a8a08d2c45b5db
Ahmed Ali <65877525+ahmedaliv@users.noreply.github.com>
2024-01-26 12:07:51 +0200

    Time: 88 ms (83.42%) | Memory: 57.6 MB (11.92%) - LeetSync

--- 221-maximal-square/maximal-square.ts
+++ 221-maximal-square/maximal-square.ts
@@ -1,6 +1,6 @@
 // the key note here is to notice the repeated work being done while checking for valid squares
 // on on each matrix cell
-// when noting this, u would go from a regualr brute-force  solution to an efficient DP solution
+// when noting this, u would go from a regular brute-force  solution to an efficient DP solution
 function maximalSquare(matrix: string[][]): number {
     
     const n:number = matrix.length;
@@ -9,30 +9,19 @@ function maximalSquare(matrix: string[][]): number {
     for (let i = 0; i < n; i++) {
         dp[i] = new Array(m).fill(0);
     }
-    function countSquares(r:number,c:number):number {
-        if(r>=n || c>=m){
-            return 0;
-        }
-        if (dp[r][c] !== 0) {
-            return dp[r][c];
-        }
-        let right = countSquares(r,c+1);
-        let down = countSquares(r+1,c);
-        let diagonal = countSquares(r+1,c+1);
-        if(matrix[r][c]=="1"){
-            // min because if any of the cells are 0 then we cannot make a square.
-            dp[r][c]= 1 + Math.min(right,down,diagonal);
-        }
-        return dp[r][c];
-    }
-    countSquares(0,0);
     let maxSize = 0;
 
     for (let i = 0; i < n; i++) {
         for (let j = 0; j < m; j++) {
-            maxSize = Math.max(maxSize, countSquares(i, j));
+            if (i === 0 || j === 0) {
+                dp[i][j] = matrix[i][j] === "1" ? 1 : 0;
+            } else if (matrix[i][j] === "1") {
+                dp[i][j] = 1 + Math.min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]);
+            }
+
+            maxSize = Math.max(maxSize, dp[i][j]);
         }
     }
 
-    return maxSize * maxSize; 
+    return maxSize * maxSize;  
 };
