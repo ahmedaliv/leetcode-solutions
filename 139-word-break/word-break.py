58d0c46b6ec93e0089c48356a6d472da08a2e429
Ahmed Ali <65877525+ahmedaliv@users.noreply.github.com>
2024-01-27 11:20:16 +0200

    Time: 17 ms (80.78%) | Memory: 11.8 MB (97.43%) - LeetSync

---
+++ 139-word-break/word-break.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+        n = len(s) 
+        dp = [False for i in range(n + 1)] 
+        dp[0] = True
+        for i in range(1, n + 1): 
+            for j in range(i): 
+                if dp[j] and s[j:i] in wordDict: 
+                    dp[i] = True
+                    break
+        return dp[n]
+        
