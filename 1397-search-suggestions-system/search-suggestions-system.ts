7cddd624061e50ba4c27921faef5d4dd46d6c3b6
Ahmed Ali <65877525+ahmedaliv@users.noreply.github.com>
2024-02-01 21:04:01 +0200

    Time: 216 ms (39.02%) | Memory: 72.8 MB (29.27%) - LeetSync

--- 1397-search-suggestions-system/search-suggestions-system.ts
+++ 1397-search-suggestions-system/search-suggestions-system.ts
@@ -1,15 +1,63 @@
-// brute force solution
+// Trie solution
+class TrieNode {
+    children: Map<string, TrieNode>;
+    suggestions: string[];
+
+    constructor() {
+        this.children = new Map();
+        this.suggestions = [];
+    }
+}
+
+class Trie {
+    root: TrieNode;
+
+    constructor() {
+        this.root = new TrieNode();
+    }
+
+    insert(word: string) {
+        let node = this.root;
+
+        for (const char of word) {
+            if (!node.children.has(char)) {
+                node.children.set(char, new TrieNode());
+            }
+
+            node = node.children.get(char)!;
+            node.suggestions.push(word);
+            node.suggestions.sort().splice(3);
+        }
+    }
+
+    search(prefix: string): string[] {
+        let node = this.root;
+
+        for (const char of prefix) {
+            if (!node.children.has(char)) {
+                return [];
+            }
+
+            node = node.children.get(char)!;
+        }
+
+        return node.suggestions;
+    }
+}
+
 function suggestedProducts(products: string[], searchWord: string): string[][] {
-    products.sort(); 
+    const trie = new Trie();
+
+    for (const product of products) {
+        trie.insert(product);
+    }
 
     const result: string[][] = [];
     let prefix = '';
 
     for (let i = 0; i < searchWord.length; i++) {
         prefix += searchWord[i];
-        
-        const suggested = products.filter(product => product.startsWith(prefix)).slice(0, 3);
-        result.push(suggested);
+        result.push(trie.search(prefix));
     }
 
     return result;
