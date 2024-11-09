72c53d83f97f8d607a954d88fd4ace523398cf9f
Ahmed Ali <ahmedmalii30@gmail.com>
2024-11-09 14:35:11 +0200

    Time: 11 ms (50.56%) | Memory: 17.5 MB (69.75%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.py
+++ 45-jump-game-ii/jump-game-ii.py
@@ -1,23 +1,39 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        n = len(nums)
-        cache = [-1] * n
+        # Greedy Approach
+        jumps = 0 
+        current_end =0 
+        farthest = 0
+        i=0
+        for i in range(len(nums)-1):
+            farthest = max(farthest,i+nums[i])
 
-        def min_jumps_from_index(index):
-            if index >= n - 1:
-                return 0
+            if i == current_end:
+                jumps +=1
+                current_end = farthest
 
-            if cache[index] != -1:
-                return cache[index]
+                if current_end >= (len(nums)-1):
+                    break
+        return jumps
+        ## recursive approach with memoization
+        # n = len(nums)
+        # cache = [-1] * n
 
-            min_jumps = float('inf')
-            max_jump = nums[index]
-            for step in range(1, max_jump + 1):
-                next_index = index + step
-                jumps_needed = 1 + min_jumps_from_index(next_index)
-                min_jumps = min(min_jumps, jumps_needed)
+        # def min_jumps_from_index(index):
+        #     if index >= n - 1:
+        #         return 0
 
-            cache[index] = min_jumps
-            return min_jumps
+        #     if cache[index] != -1:
+        #         return cache[index]
 
-        return min_jumps_from_index(0)
+        #     min_jumps = float('inf')
+        #     max_jump = nums[index]
+        #     for step in range(1, max_jump + 1):
+        #         next_index = index + step
+        #         jumps_needed = 1 + min_jumps_from_index(next_index)
+        #         min_jumps = min(min_jumps, jumps_needed)
+
+        #     cache[index] = min_jumps
+        #     return min_jumps
+
+        # return min_jumps_from_index(0)
