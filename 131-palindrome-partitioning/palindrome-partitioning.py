c6bfac1434989c732d6888bda4b158fd52f4999e
Ahmed Ali <ahmedmalii30@gmail.com>
2024-12-10 14:06:27 +0200

    Time: 40 ms (87.08%) | Memory: 34.6 MB (19.44%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -1,23 +1,24 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
+        # all results
         res = []
+        # to hold each valid partition 
         part = []
 
         def backtrack(i):
             if i >= len(s):
                 res.append(part.copy())
-                return
+                return 
 
             for j in range(i,len(s)):
                 if self.is_pal(s,i,j):
-                    part.append(s[i:j+1])
-                    backtrack(j+1)
-                    part.pop()
+                    part.append(s[i:j+1]) # do
+                    backtrack(j+1) # backtrack
+                    part.pop() # undo
         backtrack(0)
         return res
-    
     def is_pal(self,s,l,r):
-        while l < r:
+        while l<r:
             if s[l] != s[r]:
                 return False
             l,r = l+1,r-1
