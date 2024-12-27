23291901f07864f78eb3ba4d90742a70097b9e62
Ahmed Ali <ahmedmalii30@gmail.com>
2024-12-27 13:50:24 +0200

    Time: 15 ms (8.37%) | Memory: 20.1 MB (13.03%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,22 +1,20 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         dp = [[[-1 for _ in range(2)] for _ in range (3)] for _ in range(len(prices))]
-        def mx(i, lt, hs):
+        def mx(i,lt,hs):
             if i == len(prices):
-                return 0
+                return 0 
             if dp[i][lt][hs] != -1:
                 return dp[i][lt][hs]
-
-            buy,sell,no_action =0,0,0
+            dn = mx(i+1,0,hs)
+            sell,buy =0,0
+            #try buy or sell
             if hs:
-                no_action = mx(i + 1, 0, hs)
-                sell = prices[i] + mx(i + 1, 1, 0)
-                dp[i][lt][hs] =  max(sell, no_action)
+                sell = prices[i] + mx(i+1,1,0)
             else:
-                no_action = mx(i + 1, 0, hs)
-                if lt != 1:
-                    buy = -prices[i] + mx(i + 1, 2, 1)
-                    dp[i][lt][hs] = max(buy, no_action)
-                dp[i][lt][hs] = max(dp[i][lt][hs],no_action)
+                if lt!=1:
+                    buy = -prices[i] + mx(i+1,2,1) 
+            
+            dp[i][lt][hs] = max(buy,sell,dn)
             return dp[i][lt][hs]
-        return mx(0, 0, 0)
+        return mx(0,0,0)
