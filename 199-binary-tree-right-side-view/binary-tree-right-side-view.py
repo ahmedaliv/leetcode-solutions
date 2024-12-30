e177550bf1f2fdb2ee913967ac05b26d51fe3a35
Ahmed Ali <ahmedmalii30@gmail.com>
2024-12-30 14:20:21 +0200

    Time: 0 ms (100.00%) | Memory: 12.3 MB (18.02%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -11,27 +11,27 @@ class Solution(object):
 
 
         res  = []
-        def dfs(root,level):
-            if root==None:
-                return
-            if len(res) == level:
-                res.append(root.val)
-            dfs(root.right,level+1)
-            dfs(root.left,level+1)
-        dfs(root,0)
-        return res
-        
-        # if not root:
-        #     return []
-        # queue = deque([root])
-        # while len(queue) != 0:
-        #     level_len = len(queue)
-        #     for i in range(level_len):
-        #         front = queue.popleft()
-        #         if i == level_len -1:
-        #             res.append(front.val)
-        #         if front.left:
-        #             queue.append(front.left)
-        #         if front.right:
-        #             queue.append(front.right)
+        # def dfs(root,level):
+        #     if root==None:
+        #         return
+        #     if len(res) == level:
+        #         res.append(root.val)
+        #     dfs(root.right,level+1)
+        #     dfs(root.left,level+1)
+        # dfs(root,0)
         # return res
+        
+        if not root:
+            return []
+        queue = deque([root])
+        while len(queue) != 0:
+            level_len = len(queue)
+            for i in range(level_len):
+                front = queue.popleft()
+                if i == 0:
+                    res.append(front.val)
+                if front.right:
+                    queue.append(front.right)
+                if front.left:
+                    queue.append(front.left)
+        return res
