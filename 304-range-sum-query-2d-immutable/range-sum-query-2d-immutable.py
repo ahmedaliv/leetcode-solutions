7fda82a5dc360d10271c19dd2e4a75ce643c3b3b
Ahmed Ali <ahmedmalii30@gmail.com>
2025-01-16 18:05:53 +0200

    Time: 203 ms (16.95%) | Memory: 31 MB (36.30%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
@@ -3,6 +3,7 @@ class NumMatrix:
     def __init__(self, matrix: List[List[int]]):
         self.ps = copy.deepcopy(matrix)
         self.pre_row()
+        self.pre_col()
 
     def pre_row(self):
         rows = len(self.ps)
@@ -10,20 +11,20 @@ class NumMatrix:
         for r in range(rows):
             for c in range(1,cols):
                 self.ps[r][c]+=self.ps[r][c-1]
-    # def pre_col():
-    #     rows = len(self.ps)
-    #     cols = len(self.ps)
-    #     for c in range(cols):
-    #         for r
+    def pre_col(self):
+        rows = len(self.ps)
+        cols = len(self.ps[0])
+        for c in range(cols):
+            for r in range(1,rows):
+                self.ps[r][c] += self.ps[r-1][c]
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        total = 0
-        for r in range(row1,row2+1):
-            if col1:
-                total+=self.ps[r][col2]-self.ps[r][col1-1]
-            else:
-                total+=self.ps[r][col2]
-
+        total = self.ps[row2][col2]
+        total-=self.ps[row1-1][col2] if row1 >0 else 0
+        total-=self.ps[row2][col1-1]if col1>0 else 0
+        total+=self.ps[row1-1][col1-1] if row1>0  and col1 > 0 else 0
         return total
+
+
 
 # Your NumMatrix object will be instantiated and called as such:
 # obj = NumMatrix(matrix)
