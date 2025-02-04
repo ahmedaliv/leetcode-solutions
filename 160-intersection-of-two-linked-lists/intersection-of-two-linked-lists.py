cafc36d8b1e2baea203e8402c3602b69f3ec0788
Ahmed Ali <ahmedmalii30@gmail.com>
2025-02-05 00:15:13 +0200

    Time: 78 ms (86.63%) | Memory: 27.6 MB (92.42%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
@@ -5,16 +5,30 @@
 #         self.next = None
 
 class Solution:
-    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        cur_a = headA
-        cur_b = headB
-        while cur_a !=cur_b:
-            if not cur_a:
-                cur_a = headB
-            else:
-                cur_a = cur_a.next
-            if not cur_b:
-                cur_b = headA
-            else:
-                cur_b = cur_b.next
-        return cur_a 
+    def getIntersectionNode(self,headA: ListNode, headB: ListNode) -> ListNode:
+        if not headA or not headB:
+            return None
+        
+        # we created a cycle
+        tail = headA
+        while tail.next:
+            tail = tail.next
+        tail.next = headA 
+
+        # now go from headb to get the intersection ( apply fast,slow algorithm tortoise and hare )
+    
+        fast,slow = headB,headB
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if slow==fast:
+                break
+        else:
+            tail.next = None
+            return None
+        slow = headB
+        while fast != slow:
+            fast = fast.next
+            slow = slow.next
+        tail.next = None
+        return slow
