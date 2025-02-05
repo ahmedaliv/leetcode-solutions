6069a8d0bd73e9a54d20e26b052600175435e7b5
Ahmed Ali <ahmedmalii30@gmail.com>
2025-02-05 17:52:38 +0200

    Time: 685 ms (12.65%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,10 +1,7 @@
 -- Write your PostgreSQL query statement below
 
-SELECT ID
+SELECT W1.ID
 FROM WEATHER W1
-WHERE EXISTS (
-    SELECT 1 
-    FROM WEATHER W2
-    WHERE W1.recordDate - W2.recordDate = 1
-    and W1.temperature > W2.temperature
-)
+INNER JOIN WEATHER W2
+ON W1.recordDate = W2.recordDate + INTERVAL '1 day'
+AND W1.temperature > W2.temperature;
