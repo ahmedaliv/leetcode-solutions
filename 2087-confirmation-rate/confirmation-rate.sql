e0ba9a207927f027fb48d88b48c20ccbbfd6b99d
Ahmed Ali <ahmedmalii30@gmail.com>
2025-02-11 14:54:21 +0200

    Time: 459 ms (52.87%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -1,44 +1,5 @@
--- Write your PostgreSQL query statement below
-WITH TOTAL AS (
-    SELECT 
-        s.user_id,
-        COUNT(c.user_id) AS TOTAL_MESSAGES 
-    FROM 
-        SIGNUPS S
-    LEFT JOIN 
-        CONFIRMATIONS C
-    ON 
-        S.USER_ID = C.USER_ID
-    GROUP BY 
-        s.user_id
-),
-CONFIRMED AS (
-    SELECT 
-        s.user_id,
-        COUNT(c.user_id) AS CONFIRMED_MESSAGES 
-    FROM 
-        SIGNUPS S
-    LEFT JOIN 
-        CONFIRMATIONS C
-    ON 
-        S.USER_ID = C.USER_ID
-    WHERE 
-        C.ACTION = 'confirmed'
-    GROUP BY 
-        s.user_id
-)
-
-SELECT 
-    t.user_id, 
-    CASE 
-        WHEN t.TOTAL_MESSAGES = 0 THEN 0
-        ELSE ROUND(COALESCE(c.CONFIRMED_MESSAGES, 0) * 1.0 / t.TOTAL_MESSAGES, 2)
-    END AS confirmation_rate
-FROM 
-    TOTAL t
-LEFT JOIN 
-    CONFIRMED c 
-ON 
-    t.user_id = c.user_id
-ORDER BY 
-    t.user_id;
+SELECT s.user_id, 
+       ROUND(AVG(CASE WHEN c.action = 'confirmed' THEN 1 ELSE 0 END), 2) AS confirmation_rate
+FROM signups s
+LEFT JOIN confirmations c ON s.user_id = c.user_id
+GROUP BY s.user_id;
