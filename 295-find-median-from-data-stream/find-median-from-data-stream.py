2f9e7d1d2d70fde052c8c8ec682b4c22b05ebec5
Ahmed Ali <ahmedmalii30@gmail.com>
2025-02-18 11:17:11 +0200

    Time: 113 ms (88.52%) | Memory: 39.7 MB (47.28%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/find-median-from-data-stream.py
@@ -1,22 +1,34 @@
+from heapq import heappush,heappop
 class MedianFinder:
 
     def __init__(self):
-        self.med = []
+        self.left_heap = []
+        self.right_heap = []
         self.size = 0
 
     def addNum(self, num: int) -> None:
         self.size += 1
-        self.med.append(num)
+        if not self.left_heap or num <= (-self.left_heap[0]):
+            heappush(self.left_heap,-num)
+        else:
+            heappush(self.right_heap,num)
+        # maintain size property
+        if len(self.left_heap) > len(self.right_heap) + 1:
+            heappush(self.right_heap,-self.left_heap[0])
+            heappop(self.left_heap)
+        elif len(self.right_heap) > len(self.left_heap):
+            heappush(self.left_heap,-self.right_heap[0])
+            heappop(self.right_heap)
+            
 
     def findMedian(self) -> float:
-        # if odd
-        self.med.sort()
-        if self.size == 1:
-            return self.med[0]
+        # if len(self.left_heap) > len(self.right_heap):
+        #     return -self.left_heap[0]
+        # return (-self.left_heap[0] + self.right_heap[0]) / 2
         if self.size % 2 != 0:
-            return self.med[self.size // 2]
+            return -self.left_heap[0]
         else:
-            return (self.med[self.size // 2] + self.med[(self.size // 2 )-1]) / 2
+            return (-self.left_heap[0] + self.right_heap[0]) / 2
 # Your MedianFinder object will be instantiated and called as such:
 # obj = MedianFinder() 
 # obj.addNum(num)
