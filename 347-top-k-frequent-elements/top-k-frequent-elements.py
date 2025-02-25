b3686311d34acd00525fbcfc9e0d29521464b2e4
Ahmed Ali <ahmedmalii30@gmail.com>
2025-02-25 10:59:20 +0200

    Time: 7 ms (52.65%) | Memory: 24.3 MB (7.26%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,15 +1,17 @@
-from heapq import heappop,heappush
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        heap = []
-        h_m = {}
+        
+        h_m = defaultdict(int)
         for num in nums:
-            h_m[num] = h_m.get(num,0) + 1
+            h_m[num] += 1
+        
+        buckets = [[] for _ in range(len(nums) + 1)]
         for num,freq in h_m.items():
-            heappush(heap,(freq,num))
-            if len(heap)>k:
-                heappop(heap)
+            buckets[freq].append(num)
+        
         res = []
-        while heap:
-            res.append(heappop(heap)[1])
-        return res
+        for i in range(len(buckets) -1,0,-1):
+            for n in buckets[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
