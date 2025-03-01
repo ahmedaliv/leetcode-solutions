7060a5a3013a349e5d48f1d41ef4a5994464c309
Ahmed Ali <ahmedmalii30@gmail.com>
2025-03-01 15:04:53 +0200

    Time: 932 ms (54.19%) | Memory: 0B (100.00%) - LeetSync

--- 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -1,4 +1,4 @@
 SELECT customer_id
 FROM Customer
 GROUP BY customer_id
-HAVING COUNT(DISTINCT product_key) = (SELECT COUNT(*) FROM Product);
+HAVING COUNT(DISTINCT product_key) = (SELECT COUNT(product_key) FROM Product);
