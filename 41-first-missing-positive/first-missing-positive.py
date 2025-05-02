293d1b912dab66f856c38a6a951a290c6516ac27
Ahmed Ali <ahmedmalii30@gmail.com>
2025-05-02 22:31:56 +0300

    Time: 42 ms (62.19%) | Memory: 29 MB (46.83%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.py
+++ 41-first-missing-positive/first-missing-positive.py
@@ -1,14 +1,17 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
         n = len(nums)
-        i = 0
-        while i < n:
-            correct_idx = nums[i] - 1
-            if 0 < nums[i] <= n and nums[i] != nums[correct_idx]:
-                nums[correct_idx],nums[i]=nums[i],nums[correct_idx]
-            else:
-                i +=1
-        for i in range(len(nums)):
-            if nums[i] != i + 1:
+        # this is gonna be solved using cyclic sort
+        # so the algorithm is just loop over the numbers and then for each one
+        # do cycles of -replacements or movements- to move it to its place
+        # and after that check the number that's not in its correct place and return it  (or return n+1) if all in its place
+
+        for i in range(n):
+            #notice we only care about positive numbers
+            while 0 < nums[i] <= n and nums[i] != nums[nums[i]-1]:
+                nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
+        
+        for i in range(n):
+            if nums[i] != i +1:
                 return i+1
-        return n + 1
+        return n + 1 
