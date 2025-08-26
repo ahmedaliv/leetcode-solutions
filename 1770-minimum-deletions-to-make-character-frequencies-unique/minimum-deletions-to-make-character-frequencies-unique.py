73819b43f1756e5d93e86857ab42e97c0a18f548
Ahmed Ali <ahmed195131@feng.bu.edu.eg>
2025-08-26 07:18:51 +0300

    Time: 99 ms (39.58%) | Memory: 18.1 MB (55.29%) - LeetSync

--- 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.py
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.py
@@ -3,16 +3,17 @@ from collections import defaultdict
 class Solution:
     def minDeletions(self, s: str) -> int:
         deletions = 0
-        lfs = float('inf')
         h_t = defaultdict(int)
+    
         for c in s:
             h_t[c] += 1
-        freq_values = sorted(h_t.values(), reverse=True)
         
-        for f in freq_values:
-            allowed = min(f,lfs-1)
-            if allowed < 0:
-                allowed = 0
-            deletions += f - allowed
-            lfs = allowed
+        used = set() 
+        
+        for f in h_t.values():
+            while f > 0 and f in used:
+                f -= 1
+                deletions += 1
+            used.add(f)
+        
         return deletions
