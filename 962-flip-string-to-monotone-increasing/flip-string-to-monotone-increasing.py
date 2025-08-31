9800dce1ae37d3657afd6b0cc2c1434c6a17ce77
Ahmed Ali <ahmed195131@feng.bu.edu.eg>
2025-08-31 12:44:30 +0300

    Time: 228 ms (28.57%) | Memory: 25.6 MB (20.68%) - LeetSync

--- 962-flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.py
+++ 962-flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.py
@@ -1,14 +1,15 @@
 class Solution:
     def minFlipsMonoIncr(self, s: str) -> int:
-        flips = 0
-        onesCount = 0
+        n = len(s)
+        prefixOnes = [0] * (n + 1)   # +1 to handle prefixOnes[0] = 0
 
-        for c in s:
-            if c == '0':
-                if onesCount == 0:
-                    continue
-                else:
-                    flips = min(flips+1,onesCount)
-            else:
-                onesCount +=1
+        for i in range(1, n + 1):
+            prefixOnes[i] = prefixOnes[i-1] + (1 if s[i-1] == '1' else 0)
+        suffixZeros = [0] * (n + 1)  # +1 to handle suffixZeros[n] = 0
+        for i in range(n-1,-1,-1):
+            suffixZeros[i] = suffixZeros[i+1] + (1 if s[i] == '0' else 0)
+
+        flips = float('inf')
+        for i in range(n+1):
+            flips = min(flips,prefixOnes[i]+suffixZeros[i])
         return flips
