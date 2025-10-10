955ba029e1ec3769f2d62a57a701e9992a362776
Ahmed Ali <ahmed195131@feng.bu.edu.eg>
2025-10-10 07:51:19 +0300

    Time: 35 ms (46.61%) | Memory: 22 MB (11.83%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,13 +1,11 @@
+from collections import defaultdict
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        res = 0 
-        hash_map = {0:1}
-        pre_sum = 0
-        for num in nums:
-            pre_sum += num
-            # is there any subarray that by removing it we can get our target ?
-            # and if there's , how many ? (add the number to the answer)
-            if(pre_sum - k) in hash_map:
-                res += hash_map[pre_sum-k]
-            hash_map[pre_sum] = hash_map.get(pre_sum, 0) + 1
+        prefix_table = defaultdict(int)
+        prefix_table[0] = 1
+        prefix_sum = res =0
+        for i in range(len(nums)):
+            prefix_sum += nums[i]
+            res += prefix_table[prefix_sum - k] 
+            prefix_table[prefix_sum] += 1
         return res
