2fe4afb2c476483077aef8b19dddb768f1e2f35f
Ahmed Ali <ahmed195131@feng.bu.edu.eg>
2025-10-29 12:06:48 +0300

    Time: 7 ms (89.31%) | Memory: 17.7 MB (97.41%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,12 +1,4 @@
+from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(t) != len(s):
-            return False
-        freq = {}
-        for char in s:
-            freq[char] = freq.get(char, 0) + 1
-        for char in t:
-            if freq.get(char, 0) == 0:
-                return False
-            freq[char] -= 1
-        return True
+        return Counter(s) == Counter(t)
