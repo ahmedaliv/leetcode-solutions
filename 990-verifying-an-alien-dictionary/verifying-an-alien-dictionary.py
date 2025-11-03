c27c109bfb78da974ffe1391e8bb0f988b6da0a0
Ahmed Ali <ahmed195131@feng.bu.edu.eg>
2025-11-03 14:25:57 +0200

    Time: 0 ms (100.00%) | Memory: 17.8 MB (75.01%) - LeetSync

--- 990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.py
+++ 990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.py
@@ -1,16 +1,21 @@
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
-        mp = {}
-        def is_smaller(a,b):
-            l = min(len(a),len(b))
-            for i in range(l):
-                if a[i] != b[i]:
-                    return mp[a[i]] < mp[b[i]]
-            return len(a) < len(b)
+        # populate the hashmap with the order
+        idx_mp = {}
         for i in range(len(order)):
-            mp[order[i]] = i
-        
+            idx_mp[order[i]] = i
+        # custom method to define the equality logic because of the custom order we're given
+        def is_smaller(a,b):
+            cl = min(len(a),len(b))
+            for i in range(cl):
+                # for example a="word" b = "world"
+                # when we reach a-> d and b-> l we check if the order is correct 
+                if a[i] != b[i]:
+                    return idx_mp[a[i]] < idx_mp[b[i]]
+            #what if nothing happened above ?
+            # if a is smaller, return true for example a="he" b="hello"
+            return len(a) < len(b)
         for i in range(1,len(words)):
-            if words[i]!=words[i-1] and not(is_smaller(words[i-1],words[i])):
+            if words[i] != words[i-1] and (not is_smaller(words[i-1],words[i])):
                 return False
         return True
