98aac7191b5b3b2ec7ae83a4e61c596ffcbe57bc
Ahmed Ali <ahmedmalii30@gmail.com>
2025-11-04 20:50:28 +0200

    another solution to  249 - Group Shifted Strings

--- group-shifted-strings/sol.py
+++ group-shifted-strings/sol.py
@@ -3,15 +3,19 @@ from collections import defaultdict
 class Solution:
     def groupStrings(self, strings: List[str]) -> List[List[str]]:
       mp = defaultdict(list)
+      # for s in strings:
+      #   # how many chars are we from 'a'
+      #   diff = ord(s[0]) - ord('a')
+      #   t = []
+      #   for c in s:
+      #     # we shift each char by that diff(from a), so we finally get a canonical form for each string
+      #     shifted = (ord(c) - ord('a') - diff) % 26
+      #     # we convert it again to a character
+      #     t.append(chr(shifted+ord('a')))
+      #   key = ''.join(t)
+      #   mp[key].append(s)
+      # return list(mp.values())
       for s in strings:
-        # how many chars are we from 'a'
-        diff = ord(s[0]) - ord('a')
-        t = []
-        for c in s:
-          # we shift each char by that diff(from a), so we finally get a canonical form for each string
-          shifted = (ord(c) - ord('a') - diff) % 26
-          # we convert it again to a character
-          t.append(chr(shifted+ord('a')))
-        key = ''.join(t)
-        mp[key].append(s)
+          key = tuple(ord(c) - ord(s[0]) for c in s)
+          mp[key].append(s)
       return list(mp.values())
