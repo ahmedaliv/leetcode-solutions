f193fd6a3549bf01722f73c19011597e673657d1
Ahmed Ali <ahmedmalii30@gmail.com>
2025-11-04 20:34:27 +0200

    249-group-shifted-strings

---
+++ group-shifted-strings/sol.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+
+class Solution:
+    def groupStrings(self, strings: List[str]) -> List[List[str]]:
+      mp = defaultdict(list)
+      for s in strings:
+        # how many chars are we from 'a'
+        diff = ord(s[0]) - ord('a')
+        t = []
+        for c in s:
+          # we shift each char by that diff(from a), so we finally get a canonical form for each string
+          shifted = (ord(c) - ord('a') - diff) % 26
+          # we convert it again to a character
+          t.append(chr(shifted+ord('a')))
+        key = ''.join(t)
+        mp[key].append(s)
+      return list(mp.values())
