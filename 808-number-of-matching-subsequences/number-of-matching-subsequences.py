4584b07b9e2ec0133a3b19f190d0666216e4a97d
Ahmed Ali <ahmed195131@feng.bu.edu.eg>
2025-11-05 15:00:11 +0200

    Time: 280 ms (57.14%) | Memory: 19.5 MB (82.92%) - LeetSync

--- 808-number-of-matching-subsequences/number-of-matching-subsequences.py
+++ 808-number-of-matching-subsequences/number-of-matching-subsequences.py
@@ -1,20 +1,19 @@
+from collections import defaultdict
 class Solution:
     def numMatchingSubseq(self, s: str, words: List[str]) -> int:
-        heads = [[] for _ in range(26)]
-        for word in words:
-            it = iter(word)
-            heads[ord(next(it))-ord('a')].append(it)
-        
-        ans = 0
+        letter_to_indices = defaultdict(list)
+        for i,w in enumerate(words):
+            letter_to_indices[w[0]].append(i)
+        pos = [0]*len(words)
+        matched_words = 0
         for c in s:
-            c_idx = ord(c)-ord('a')
-            old_list = heads[c_idx]
-            heads[c_idx] = []
-            while old_list:
-                it = old_list.pop()
-                nxt_char = next(it,None)
-                if nxt_char:
-                    heads[ord(nxt_char)-ord('a')].append(it)
+            word_indices = letter_to_indices[c][:]
+            letter_to_indices[c].clear()
+            for w_i in word_indices:
+                word = words[w_i]
+                pos[w_i] +=1
+                if pos[w_i] == len(word):
+                    matched_words +=1
                 else:
-                    ans+=1
-        return ans
+                    letter_to_indices[word[pos[w_i]]].append(w_i)
+        return matched_words
