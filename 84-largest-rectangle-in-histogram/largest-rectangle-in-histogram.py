13a16a2e4f12c01c860f38e5d51c4fe83b31b290
Ahmed Mohamed <ahmed195131@feng.bu.edu.eg>
2025-11-12 16:47:52 +0200

    Time: 240 ms (12.40%) | Memory: 31.8 MB (40.73%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -1,36 +1,39 @@
-from typing import List
-
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
+        #Brute Force O(N^2)
+        # best = 0
+        # n = len(heights)
+        # for i in range(n):
+        #     min_height = heights[i]
+        #     for j in range(i,n):
+        #         min_height = min(min_height,heights[j])
+        #         best = max(best, min_height * (j - i + 1))
+        # return best
         n = len(heights)
-        
-        def next_lower():
-            res = [n] * n  
-            s = []
-            for i in range(n):
-                while s and heights[s[-1]] > heights[i]:
-                    res[s[-1]] = i
-                    s.pop()
-                s.append(i)
-            return res
-        
-        def prev_lower():
-            res = [-1] * n 
-            s = []
-            for i in range(n-1, -1, -1):
-                while s and heights[s[-1]] > heights[i]:
-                    res[s[-1]] = i
-                    s.pop()
-                s.append(i)
-            return res
-        
-        nl = next_lower()
-        pl = prev_lower()
-        
+        nx = self.next_lower_idx(heights)
+        pv = self.prev_lower_idx(heights)
         best = 0
         for i in range(n):
-            left = pl[i] + 1
-            right = nl[i] - 1
-            best = max(best, heights[i] * (right - left + 1))
-        
+            l = pv[i] + 1 
+            r = nx[i] - 1
+            best = max(best,heights[i]*(r-l+1))
         return best
+    def next_lower_idx(self, arr):
+        n = len(arr)
+        stk = []
+        res = [n] * n
+        for i in range(n):
+            while stk and arr[stk[-1]] > arr[i]:
+                res[stk[-1]] = i 
+                stk.pop()
+            stk.append(i)
+        return res
+    def prev_lower_idx(self,arr):
+        res = arr[::]
+        res.reverse()
+        res = self.next_lower_idx(res)
+        res.reverse()
+        n = len(res)
+        for i in range(n):
+            res[i] = n - res[i] - 1
+        return res
