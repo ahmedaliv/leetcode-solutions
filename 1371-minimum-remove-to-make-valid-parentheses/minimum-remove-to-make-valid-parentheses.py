6cbdb71fb29a2c473a96ae69e0a408d833dd43e2
Ahmed Mohamed <ahmed195131@feng.bu.edu.eg>
2025-11-15 21:02:36 +0200

    Time: 44 ms (50.85%) | Memory: 19.2 MB (60.55%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -1,19 +1,14 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        open_idxs = []
-        close_idxs = []
-        res = ""
+        stk = []
+        remove = set()
         for i,c in enumerate(s):
-            if c == "(":
-                open_idxs.append(i)
-            elif c == ")":
-                if open_idxs:
-                    open_idxs.pop()
+            if c == '(':
+                stk.append(i)
+            elif c == ')':
+                if stk:
+                    stk.pop()
                 else:
-                    close_idxs.append(i)
-        ans = ""
-        for i in range(len(s)):
-            if i in open_idxs or i in close_idxs:
-                continue
-            ans += s[i]
-        return ans
+                    remove.add(i)
+        remove = remove.union(set(stk))
+        return ''.join(c for i,c in enumerate(s) if i not in remove)
