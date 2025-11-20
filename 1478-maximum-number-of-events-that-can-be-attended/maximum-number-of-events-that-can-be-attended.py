bf291bdc457781c508f1741261d1394e3def0703
Ahmed Mohamed <ahmed195131@feng.bu.edu.eg>
2025-11-20 20:20:49 +0200

    Time: 132 ms (79.30%) | Memory: 53.1 MB (95.29%) - LeetSync

--- 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.py
+++ 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.py
@@ -13,8 +13,8 @@ class Solution:
                 heappop(mnHeap)
             # optimization (jump to the next event if there are no ones left) 
             # instead of moving 1 by 1
-            # if event < sz and len(mnHeap) == 0:
-            #     day = max(day,events[event][0])
+            if event < sz and len(mnHeap) == 0:
+                day = max(day,events[event][0])
             # add active events today
             while event < sz and events[event][0] == day:
                 heappush(mnHeap,events[event][1])
