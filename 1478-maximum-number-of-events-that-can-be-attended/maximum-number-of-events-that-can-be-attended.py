84811ad56418a272d7d117b6d4bf1c75b2af0d39
Ahmed Mohamed <ahmed195131@feng.bu.edu.eg>
2025-11-20 20:20:41 +0200

    Time: 151 ms (54.92%) | Memory: 53.2 MB (58.18%) - LeetSync

--- 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.py
+++ 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.py
@@ -1,25 +1,29 @@
-from sortedcontainers import SortedSet
-
+from heapq import heappush,heappop
 class Solution:
-    def cmp(self, a: List[int], b: List[int]) -> bool:
-        if a[1] < b[1]:
-            return True
-        elif a[1] == b[1]:
-            return a[0] < b[0]
-        return False
-
     def maxEvents(self, events: List[List[int]]) -> int:
-        events.sort(key=functools.cmp_to_key(lambda a, b: -1 if self.cmp(a, b) else 1))
-        days = SortedSet()
-        cnt = 0
-        
-        for i in range(1, 100001):
-            days.add(i)
+        events.sort()  
+        event = 0
+        attended = 0
+        sz = len(events)
+        day = 1
+        mnHeap = []
+        while event < sz or mnHeap:
+            # remove finished events
+            while len(mnHeap) > 0 and mnHeap[0] < day:
+                heappop(mnHeap)
+            # optimization (jump to the next event if there are no ones left) 
+            # instead of moving 1 by 1
+            # if event < sz and len(mnHeap) == 0:
+            #     day = max(day,events[event][0])
+            # add active events today
+            while event < sz and events[event][0] == day:
+                heappush(mnHeap,events[event][1])
+                event +=1
+
             
-        for event in events:
-            it = days.bisect_left(event[0])
-            if it < len(days) and days[it] <= event[1]:
-                days.remove(days[it])
-                cnt += 1
-                
-        return cnt
+            # attend the one ending first
+            if len(mnHeap) > 0:
+                attended +=1
+                heappop(mnHeap)
+            day+=1
+        return attended
