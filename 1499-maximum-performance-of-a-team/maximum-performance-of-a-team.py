7789d9f644f83122d8973e00aa170cfe14a94e59
Ahmed Mohamed <ahmed195131@feng.bu.edu.eg>
2025-11-23 18:37:23 +0200

    Time: 108 ms (21.10%) | Memory: 35.2 MB (57.14%) - LeetSync

--- 1499-maximum-performance-of-a-team/maximum-performance-of-a-team.py
+++ 1499-maximum-performance-of-a-team/maximum-performance-of-a-team.py
@@ -1,22 +1,18 @@
-from heapq import heappush, heappop
+import heapq
 class Solution:
     def maxPerformance(self, n: int, speed: List[int], efficiency: List[int], k: int) -> int:
-        eng = []
-        for i in range(len(speed)):
-            eng.append([efficiency[i], speed[i]])
-        eng.sort(reverse=True)
-        
-        best_team = []
-        ans = 0
-        cur_sum = 0
-        
-        for e, s in eng:
-            cur_sum += s
-            heappush(best_team, s)
-            
-            if len(best_team) > k:
-                cur_sum -= heappop(best_team)
-            
-            ans = max(ans, cur_sum * e)
-        
-        return ans % (10**9 + 7)
+        moduls = 10**9 + 7
+        ans = 0 
+        eng = sorted(zip(efficiency,speed),reverse=True)
+        heap = []
+        speedsum = 0
+        for i in range(len(eng)):
+            ef = eng[i][0]
+            sp = eng[i][1]
+            speedsum +=sp
+            heapq.heappush(heap,sp)
+            if len(heap) > k :
+                speedsum -= heapq.heappop(heap)
+                # ef = 
+            ans = max(ans,speedsum*ef)
+        return ans % moduls
