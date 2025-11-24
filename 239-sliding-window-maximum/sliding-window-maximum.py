cedf604962af367a783c96e6c8e9330920c347f7
Ahmed Mohamed <ahmed195131@feng.bu.edu.eg>
2025-11-24 08:58:05 +0200

    Time: 183 ms (39.61%) | Memory: 31.3 MB (64.59%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -1,16 +1,22 @@
-from heapq import heappop, heappush
-# from collections import deque
+# from heapq import heappop, heappush
+from collections import deque
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         res = []
-        max_heap = []
+        dq = deque()
         for i,num in enumerate(nums):
-            heappush(max_heap,(-nums[i],i))
-
-            # remove all elements outside the window
-            while max_heap[0][1] <= i-k:
-                heappop(max_heap)
-            # if we're done with the window
+            # remove out of bound
+            # i-k+1 is the start of the window
+            # let's say our start was 1 and we have k=3 and i=3
+            # so that means 3-3+1 = 1 so anything below 1 should be removed
+            while dq and dq[0] < i-k+1:
+                dq.popleft()
+            
+            # remove anything smaller than the current element
+            # because we want to always maintain the front to be the max, so we remove anything smaller from the back
+            while dq and nums[dq[-1]] < num:
+                dq.pop()
+            dq.append(i)
             if i >= k-1:
-                res.append(-max_heap[0][0])
+                res.append(nums[dq[0]])
         return res
