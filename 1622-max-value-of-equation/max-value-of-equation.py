112bd50b14444977f0a8f02b58ace6ee1a3fdf50
Ahmed Mohamed <ahmed195131@feng.bu.edu.eg>
2025-11-25 09:53:56 +0200

    Time: 93 ms (53.22%) | Memory: 55.7 MB (9.36%) - LeetSync

--- 1622-max-value-of-equation/max-value-of-equation.py
+++ 1622-max-value-of-equation/max-value-of-equation.py
@@ -1,15 +1,13 @@
 from heapq import heappush,heappop
 class Solution:
     def findMaxValueOfEquation(self, points: List[List[int]], k: int) -> int:
-        heap = []
+        pq = []
+        ans = float('-inf')
+        for x,y in points:
+            while pq and x - pq[0][1]>k:
+                heappop(pq)
+            if pq:
+                ans = max(ans,-pq[0][0] + x + y)
 
-        best = float('-inf')
-        heappush(heap,(-(points[0][1]-points[0][0]),points[0][0]))
-        for i in range(1,len(points)):
-            while heap and abs(heap[0][1] - points[i][0]) > k:
-                heappop(heap)
-            if heap:
-                cur_val = points[i][0]+points[i][1]
-                best = max(best,cur_val-heap[0][0])
-            heappush(heap,(-(points[i][1]-points[i][0]),points[i][0]))
-        return best
+            heappush(pq,(-(y-x),x))
+        return ans
