bf432dbd17a645664d1b7c33319627b0d780400f
Ahmed Mohamed <ahmed195131@feng.bu.edu.eg>
2025-11-25 10:05:24 +0200

    Time: 86 ms (68.42%) | Memory: 55.2 MB (94.74%) - LeetSync

--- 1622-max-value-of-equation/max-value-of-equation.py
+++ 1622-max-value-of-equation/max-value-of-equation.py
@@ -1,13 +1,18 @@
-from heapq import heappush,heappop
+# from heapq import heappush,heappop
+from collections import deque
 class Solution:
     def findMaxValueOfEquation(self, points: List[List[int]], k: int) -> int:
-        pq = []
+        dq = deque()
         ans = float('-inf')
         for x,y in points:
-            while pq and x - pq[0][1]>k:
-                heappop(pq)
-            if pq:
-                ans = max(ans,-pq[0][0] + x + y)
-
-            heappush(pq,(-(y-x),x))
+            # remove invalid entires
+            while dq and x - dq[0][1] > k:
+                dq.popleft()
+            # maximize on the answer
+            if dq:
+                ans = max(ans,dq[0][0] + x + y)
+            # remove anything smaller than us 
+            while dq and dq[-1][0] < y-x:
+                dq.pop()
+            dq.append(((y-x),x))
         return ans
