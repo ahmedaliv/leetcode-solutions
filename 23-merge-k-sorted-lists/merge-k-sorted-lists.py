6ab81fb4aebeb481b0794636c2b113632d1f57ba
Ahmed Mohamed <ahmed195131@feng.bu.edu.eg>
2025-12-07 12:50:46 +0200

    Time: 8 ms (75.42%) | Memory: 20.3 MB (36.60%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -1,31 +1,27 @@
-from heapq import heappush,heappop
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+from heapq import heappush, heappop
+from itertools import count
+
+
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        
-        """
-        we would put a pointer in the start of each list 
-        and then get the one that has the smallest value and then append it to the result
-        and then advance 
-        and so on
-        
-        """
         pointers = []
+        counter = count()
         for i,l in enumerate(lists):
             if l:
-                heappush(pointers,(l.val,i,l))
-        dummy = ListNode()
-        res_tail = dummy
+                heappush(pointers,(l.val,next(counter),l))
+        dummy = ListNode(-1)
+        tail = dummy
         while pointers:
-            # get the pointer the points to the smallest value
-            _,i,smol_pointer = heappop(pointers)
-            res_tail.next = smol_pointer
-            res_tail = res_tail.next
-            if smol_pointer.next:
-                heappush(pointers,(smol_pointer.next.val,i,smol_pointer.next))
-            
+            val,cnt,top = heappop(pointers)
+            if top.next:
+                heappush(pointers,(top.next.val,next(counter),top.next))
+            tail.next = top
+            tail = tail.next
         return dummy.next
+
+
