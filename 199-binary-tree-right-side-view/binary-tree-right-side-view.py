81c9a5dade1e40b343b5c53f49314d696b0630cd
Ahmed Mohamed <ahmed195131@feng.bu.edu.eg>
2025-12-09 14:53:12 +0200

    Time: 0 ms (100.00%) | Memory: 17.7 MB (87.65%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -1,37 +1,26 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-from collections import deque
-class Solution(object):
-
-    def rightSideView(self, root):
-
-
-        res  = []
-        # def dfs(root,level):
-        #     if root==None:
-        #         return
-        #     if len(res) == level:
-        #         res.append(root.val)
-        #     dfs(root.right,level+1)
-        #     dfs(root.left,level+1)
-        # dfs(root,0)
-        # return res
-        
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        #simple BFS solution
+        # we go level by level (but by adding nodes from right to left)
+        # so the first node in each level is the first one to the right 
         if not root:
             return []
+        res = []
         queue = deque([root])
-        while len(queue) != 0:
-            level_len = len(queue)
-            for i in range(level_len):
+        while len(queue):
+            level_size = len(queue)
+            for i in range(level_size):
                 front = queue.popleft()
-                if i == 0:
-                    res.append(front.val)
+                if not i:
+                    res.append(front.val) 
                 if front.right:
                     queue.append(front.right)
                 if front.left:
-                    queue.append(front.left)
+                    queue.append(front.left)    
         return res
