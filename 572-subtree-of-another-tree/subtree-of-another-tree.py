3f5f23c620cf038055b37d521e276ec14ba3bcb9
Ahmed Mohamed <ahmed195131@feng.bu.edu.eg>
2025-12-14 15:44:54 +0200

    Time: 11 ms (94.94%) | Memory: 21.3 MB (5.74%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/subtree-of-another-tree.py
@@ -5,25 +5,23 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def is_same(self,root,subRoot):
-        if not root and not subRoot:
-            return True
-        if not root or not subRoot:
-            return False
-        if root.val != subRoot.val:
-            return False
-        return self.is_same(root.left,subRoot.left) and self.is_same(root.right,subRoot.right)
-
         
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        # we have 3 cases, either the cur. root is the same as the target subtree
-        # or the subtree on the left or in the right
-        if not root and not subRoot:
-            return True 
-        if not root or not subRoot:
-            return False
-        if self.is_same(root,subRoot):
-            return True
-        return self.isSubtree(root.left,subRoot) or self.isSubtree(root.right,subRoot)
+        SEEN = set()
+        def dfs(root):
+            if not root:
+                return '#'
 
+            left = dfs(root.left)
+            right = dfs(root.right)
 
+            h = f"{root.val},{left},{right}"
+            SEEN.add(h)
+            return h
+        def dfs_sub(root):
+            if not root:
+                return '#'
+            return f"{root.val},{dfs_sub(root.left)},{dfs_sub(root.right)}"
+            
+        dfs(root)
+        return dfs_sub(subRoot) in SEEN
