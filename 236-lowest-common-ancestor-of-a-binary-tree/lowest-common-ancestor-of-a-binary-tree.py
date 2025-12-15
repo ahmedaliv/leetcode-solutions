eb0e96a56d4ede71962f806e4cce1bb0aa9a97ba
Ahmed Mohamed <ahmed195131@feng.bu.edu.eg>
2025-12-15 15:31:37 +0200

    Time: 45 ms (93.03%) | Memory: 22.1 MB (43.49%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -7,34 +7,12 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        parent = {root: None}
-        depth = {root: 0}
-
-        # build parent and depth
-        stack = [root]
-        while stack:
-            node = stack.pop()
-
-            if node.left:
-                parent[node.left] = node
-                depth[node.left] = depth[node] + 1
-                stack.append(node.left)
-
-            if node.right:
-                parent[node.right] = node
-                depth[node.right] = depth[node] + 1
-                stack.append(node.right)
-
-        # align depths
-        while depth[p] > depth[q]:
-            p = parent[p]
-
-        while depth[q] > depth[p]:
-            q = parent[q]
-
-        # move up together
-        while p != q:
-            p = parent[p]
-            q = parent[q]
-
-        return p
+        if not root or root == p or root == q:
+            return root
+        l = self.lowestCommonAncestor(root.left,p,q)
+        r = self.lowestCommonAncestor(root.right,p,q)
+        if l and r:
+            return root
+        if l:
+            return l
+        return r
