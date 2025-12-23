77715c61242b6087ef26da84129d043f479b274c
Ahmed Mohamed <ahmed195131@feng.bu.edu.eg>
2025-12-23 18:36:54 +0200

    Time: 0 ms (100.00%) | Memory: 17.3 MB (98.41%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.py
+++ 101-symmetric-tree/symmetric-tree.py
@@ -4,13 +4,22 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
+
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        def is_mirror(t1,t2):
+        if not root:
+            return True
+        
+        queue = deque([(root.left, root.right)])
+        while queue:
+            t1,t2 = queue.popleft()
             if not t1 and not t2:
-                return True
+                continue
             if not t1 or not t2:
                 return False
-            
-            return (t1.val==t2.val) and is_mirror(t1.left,t2.right) and is_mirror(t1.right,t2.left)
-        return is_mirror(root.left,root.right)
+            if t1.val != t2.val:
+                return False
+            queue.append((t1.left,t2.right))
+            queue.append((t1.right,t2.left))
+        return True
