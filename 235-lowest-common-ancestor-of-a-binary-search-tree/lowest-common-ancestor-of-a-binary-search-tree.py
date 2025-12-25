79ae2183935cbffed7667ad6080ed2be2c8eaee8
Ahmed Mohamed <ahmed195131@feng.bu.edu.eg>
2025-12-25 15:10:55 +0200

    Time: 57 ms (62.95%) | Memory: 20.7 MB (95.58%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,24 +7,12 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        
-        if not root or p == root or q==root:
+        if not root or root == p or root == q:
             return root
-        
-        # check values to know where to search
-        # if both are lower than root we go right if not we go left
-        # or if not we search in both 
-        left = None
-        right = None
-        if p.val < root.val and q.val < root.val:
-            left = self.lowestCommonAncestor(root.left,p,q)
-        elif p.val > root.val and q.val>root.val:
-            right = self.lowestCommonAncestor(root.right,p,q)
+
+        if p.val > root.val and q.val > root.val:
+            return self.lowestCommonAncestor(root.right,p,q)
+        elif p.val < root.val and q.val < root.val:
+            return self.lowestCommonAncestor(root.left,p,q)
         else:
-            left = self.lowestCommonAncestor(root.left,p,q)
-            right = self.lowestCommonAncestor(root.right,p,q)
-        if not left:
-            return right
-        if not right:
-            return left
-        return root
+            return root
