e4bce44c33c0d7aa3d477d4f4ff9d8ab47f31e7f
Ahmed Mohamed <ahmed195131@feng.bu.edu.eg>
2025-12-25 15:33:45 +0200

    Time: 0 ms (100.00%) | Memory: 17.5 MB (93.56%) - LeetSync

--- 988-flip-equivalent-binary-trees/flip-equivalent-binary-trees.py
+++ 988-flip-equivalent-binary-trees/flip-equivalent-binary-trees.py
@@ -1,19 +1,12 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
-    def flipEquiv(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
-        def parethesize(root):
+    def flipEquiv(self, root1, root2):
+        def canon(root):
             if not root:
-                return '()'
-            left = parethesize(root.left)
-            right = parethesize(root.right)
-            if left>right:
-                left,right=right,left
-            return f'{root.val},{left},{right}'
-        canonical_1 = parethesize(root1)
-        canonical_2 = parethesize(root2)
-        return canonical_1 == canonical_2
+                return ()
+            left = canon(root.left)
+            right = canon(root.right)
+            if left > right:
+                left, right = right, left
+            return (root.val, left, right)
+
+        return canon(root1) == canon(root2)
