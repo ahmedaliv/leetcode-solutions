48c15918a9d196bda12c12929f4abfdac93ef569
Ahmed Mohamed <ahmed195131@feng.bu.edu.eg>
2025-12-25 15:25:06 +0200

    Time: 0 ms (100.00%) | Memory: 17.1 MB (99.41%) - LeetSync

--- 988-flip-equivalent-binary-trees/flip-equivalent-binary-trees.py
+++ 988-flip-equivalent-binary-trees/flip-equivalent-binary-trees.py
@@ -4,21 +4,16 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
 class Solution:
     def flipEquiv(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
-        def check_equiv(r1, r2):
-            if not r1 and not r2:
-                return True
-            if not r1 or not r2 or r1.val != r2.val:
-                return False
-            
-            if check_equiv(r1.left, r2.left) and check_equiv(r1.right, r2.right):
-                return True
-            
-            if check_equiv(r1.left, r2.right) and check_equiv(r1.right, r2.left):
-                return True
-            
-            return False
-        
-        return check_equiv(root1, root2)
+        def parethesize(root):
+            if not root:
+                return '()'
+            left = parethesize(root.left)
+            right = parethesize(root.right)
+            if left>right:
+                left,right=right,left
+            return f'{root.val},{left},{right}'
+        canonical_1 = parethesize(root1)
+        canonical_2 = parethesize(root2)
+        return canonical_1 == canonical_2
