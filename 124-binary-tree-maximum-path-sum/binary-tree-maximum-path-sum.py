466849d9f21d0b222212fd5249698516316fc969
Ahmed Mohamed <ahmed195131@feng.bu.edu.eg>
2026-01-09 07:29:20 +0200

    Time: 11 ms (90.10%) | Memory: 24.1 MB (5.04%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.py
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.py
@@ -7,24 +7,16 @@
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
         max_so_far = float('-inf')
-        def mx(root):
+        def max_sum_starting_from_node(root):
+            nonlocal max_so_far
             if not root:
                 return 0
-            # get max of my left and right  children
-            left =  mx(root.left)
-            right = mx(root.right)
-            nonlocal max_so_far
-            # 4 cases for maximization 
-            # only me
-            max_so_far = max(max_so_far,root.val)
-            # me and my the left 
-            max_so_far = max(max_so_far,root.val + left)
-            # me and my right 
-            max_so_far = max(max_so_far,root.val + right)
-            # me and both my left and right
-            max_so_far = max(max_so_far,root.val + left + right)
+            l = max_sum_starting_from_node(root.left)
+            r = max_sum_starting_from_node(root.right)
 
-            # when returning, we can only return 3 cases (the ones that include the current node)
-            return max(root.val,root.val + max(left,right))
-        mx(root)
+            max_so_far=max(max_so_far,root.val,root.val+l+r,root.val+l,root.val+r)
+            
+            # 3 cases for max sum starting with this root
+            return root.val + max(0,l,r)
+        max_sum_starting_from_node(root)
         return max_so_far
